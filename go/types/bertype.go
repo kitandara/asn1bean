df8017e1c51a76fac943c9c9b24ad65ef5b66a19
Paul Bagyenda <bagyenda@dsmagic.com>
2024-10-14 16:14:30 +0300

    - added initial golang lib files

---
+++ go/types/bertype.go
@@ -0,0 +1,10 @@
+package asn1
+
+import "io"
+
+// BerType Base type for BerTypes
+type BerType interface {
+	Encode(reversedWriter io.Writer, withTagList ...bool) (int, error)
+	Decode(r io.Reader, withTagList ...bool) (int, error)
+	S() string
+}
